9df499f7cddf85a9b4bed00f90895ca7f40e7f7d
David Gómez <dagove47@gmail.com>
2023-05-10 15:00:25 -0600

    Assets folder and favicon added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-
-const clockId = document.getElementById('clock');
-const stopWatchId = document.getElementById('stopWatch');
-const timerId = document.getElementById('timer');
-const options = document.querySelector('.options');
-const arrow = document.getElementsByClassName('arrow');
+const clockId = document.getElementById("clock");
+const stopWatchId = document.getElementById("stopWatch");
+const timerId = document.getElementById("timer");
+const options = document.querySelector(".options");
+const arrow = document.getElementsByClassName("arrow");
 const number = document.querySelector(".number");
-const numHours = document.getElementById('numHours');
-const numMinutes = document.getElementById('numMinutes');
-const numSeconds = document.getElementById('numSeconds');
-const restart = document.querySelector('.restart');
-const pause = document.querySelector('.pause');
-const start = document.querySelector('.start');
+const numHours = document.getElementById("numHours");
+const numMinutes = document.getElementById("numMinutes");
+const numSeconds = document.getElementById("numSeconds");
+const restart = document.querySelector(".restart");
+const pause = document.querySelector(".pause");
+const start = document.querySelector(".start");
 
 let hours = 0;
 let minutes = 0;
@@ -24,14 +23,23 @@ let isPause = true;
 function clock() {
     clearIntervals();
     removeActiveOpt();
-    clockId.classList.add('activeOpt');
+    clockId.classList.add("activeOpt");
     removeArrows();
     removeFuncBts();
 
-    clockInterval = setInterval(function() {
-        hours = (new Date().getHours() > 9) ?  new Date().getHours() : ('0' + new Date().getHours());
-        minutes = (new Date().getMinutes() > 9) ?  new Date().getMinutes() : ('0' + new Date().getMinutes());
-        seconds = (new Date().getSeconds() > 9) ?  new Date().getSeconds() : ('0' + new Date().getSeconds());
+    clockInterval = setInterval(function () {
+        hours =
+            new Date().getHours() > 9
+                ? new Date().getHours()
+                : "0" + new Date().getHours();
+        minutes =
+            new Date().getMinutes() > 9
+                ? new Date().getMinutes()
+                : "0" + new Date().getMinutes();
+        seconds =
+            new Date().getSeconds() > 9
+                ? new Date().getSeconds()
+                : "0" + new Date().getSeconds();
         insertTime();
     }, 100);
 }
@@ -39,27 +47,26 @@ function clock() {
 function stopWatch() {
     clearIntervals();
     removeActiveOpt();
-    stopWatchId.classList.add('activeOpt');
+    stopWatchId.classList.add("activeOpt");
     removeArrows();
     removeFuncBts();
 
     stopWatchInterval = setInterval(() => {
         if (!isPause) {
             if (totalSec % 60 === 0) {
-                minutes >= 59? (minutes = 0) : minutes++;
-                minutes = minutes > 9 ? minutes : ('0' + minutes);
+                minutes >= 59 ? (minutes = 0) : minutes++;
+                minutes = minutes > 9 ? minutes : "0" + minutes;
             }
             if (totalSec % 3600 === 0) {
-                hours >= 24? (hours = 0) : hours++;
-                hours = hours > 9 ? hours : ('0' + hours);
+                hours >= 24 ? (hours = 0) : hours++;
+                hours = hours > 9 ? hours : "0" + hours;
             }
-            seconds >= 59? (seconds = 0) : seconds++;
-            seconds = seconds > 9 ? seconds : ('0' + seconds);
+            seconds >= 59 ? (seconds = 0) : seconds++;
+            seconds = seconds > 9 ? seconds : "0" + seconds;
             totalSec++;
             insertTime();
         }
     }, 1000);
-
 }
 
 function pauseTime() {
@@ -83,9 +90,9 @@ function restartTime() {
     minutes = 0;
     seconds = 0;
     totalSec = 1;
-    hours = hours > 9 ? hours : ('0' + hours);
-    minutes = minutes > 9 ? minutes : ('0' + minutes);
-    seconds = seconds > 9 ? seconds : ('0' + seconds);
+    hours = hours > 9 ? hours : "0" + hours;
+    minutes = minutes > 9 ? minutes : "0" + minutes;
+    seconds = seconds > 9 ? seconds : "0" + seconds;
     insertTime();
 }
 
@@ -98,53 +105,53 @@ function insertTime() {
 function timer() {
     clearIntervals();
     removeActiveOpt();
-    timerId.classList.add('activeOpt');
+    timerId.classList.add("activeOpt");
     removeArrows();
     removeFuncBts();
     stopClock();
 }
 
 function removeActiveOpt() {
-    clockId.classList.remove('activeOpt');
-    stopWatchId.classList.remove('activeOpt');
-    timerId.classList.remove('activeOpt');   
+    clockId.classList.remove("activeOpt");
+    stopWatchId.classList.remove("activeOpt");
+    timerId.classList.remove("activeOpt");
 }
 
 function removeArrows() {
-    if (timerId.classList.contains('activeOpt')) {
+    if (timerId.classList.contains("activeOpt")) {
         for (let i = 0; arrow.length > i; i++) {
-            arrow[i].classList.remove('hide');
+            arrow[i].classList.remove("hide");
         }
     } else {
         for (let i = 0; arrow.length > i; i++) {
-            arrow[i].classList.add('hide');
+            arrow[i].classList.add("hide");
         }
     }
 }
 
 function removeFuncBts() {
-    if (clockId.classList.contains('activeOpt')) {
-        restart.classList.add('hide');
-        start.classList.add('hide');
-        pause.classList.add('hide');
+    if (clockId.classList.contains("activeOpt")) {
+        restart.classList.add("hide");
+        start.classList.add("hide");
+        pause.classList.add("hide");
     } else {
-        restart.classList.remove('hide');
-        start.classList.remove('hide');
-        pause.classList.remove('hide');
+        restart.classList.remove("hide");
+        start.classList.remove("hide");
+        pause.classList.remove("hide");
     }
 }
 
 function increaseNum() {
     let numId = window.event.target.nextElementSibling.id;
     switch (numId) {
-        case 'numHours':
-            console.log('test');
+        case "numHours":
+            console.log("test");
             console.log(new Date().getMinutes());
             break;
-        case 'numMinutes':
+        case "numMinutes":
             // sss
             break;
-        case 'numSeconds':
+        case "numSeconds":
             // sss
             break;
     }
@@ -153,22 +160,9 @@ function increaseNum() {
     console.log(window.event.target.nextElementSibling.id);
 }
 
-
 function decreaseNum() {
     console.log(window.event.target.previousElementSibling);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 clock();
 
@@ -176,7 +170,3 @@ clock();
 //     // number[0].textContent = "1";
 //     console.log(upArrow.childElementCount);
 // })
-
-
-
-
